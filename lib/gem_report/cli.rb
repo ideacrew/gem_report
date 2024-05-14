8dd6e8a808afcb1bd0f084101b98409a7e642f22
Trey Evans <lewis.r.evans@gmail.com>
2024-05-14 13:23:02 -0400

    Add binary.

--- lib/gem_report/cli.rb
+++ lib/gem_report/cli.rb
@@ -2,16 +2,25 @@ module GemReport
   class Cli
     def self.run
       if ARGV.empty? || ARGV[1].nil?
-        STDERR.print "Usage: gem_reporter [project_name] [sha]\n\n"
+        usage
         exit(1)
       end
       
-      data = STDIN.read
+      if STDIN.tty?
+        usage
+        exit(1)
+      end
 
+      data = STDIN.read
 
       inventory = Bundler::LockfileParser.new(data)
       csv_report = GemReport::Reports::CsvReport.new(ARGV[0], ARGV[1])
       csv_report.report(STDOUT, inventory)
+    end
+
+    def self.usage
+      STDERR.print "\nUsage: gem_reporter [project_name] [sha]\n\n"
+      STDERR.print "       Reads from STDIN by default, so make sure to provide the Gemfile.lock.\n\n"
     end
   end
 end
